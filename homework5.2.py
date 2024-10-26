6bca82138a5fa071a24054e73e66e7c8ea490e01
Mikhail Korchak <50641041+MishaKorchak@users.noreply.github.com>
2024-10-27 00:14:25 +0300

    Add files via upload

---
+++ homework5.2.py
@@ -0,0 +1,32 @@
+from operator import index
+
+from test import search_criteria
+
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+new_person = ('Misha', 'Korchal', 28, 'Engineer', 'Kiev')
+
+people_records.insert(index(0),new_person)
+
+people_records[1],people_records[5] = people_records[5],people_records[1]
+print(people_records)
+
+age =all(people_records[i][2] >= 30 for i in [6, 10, 13])
+
+print(age)
